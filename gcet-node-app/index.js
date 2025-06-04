526f7ff0e7f679f92bbccc0f72baceb8067b7414
23R11A6719 <23r11a6719@gcet.edu.in>
2025-06-04 11:57:07 +0530

    update

---
+++ gcet-node-app/index.js
@@ -0,0 +1,77 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.listen(8080, () => {
+    mongoose.connect("mongodb://localhost:27017/gcet");
+  console.log("Server Started on port 8080");
+});
+
+const userSchema = mongoose.Schema({
+    name: {type: String},
+});
+
+const user = mongoose.model("User", userSchema);
+
+app.use(cors())
+app.use(express.json())
+app.get("/", async (req, res) => {
+  return res.send("Good Morning");
+});
+
+app.post("/register", async(req, res)=>{
+    const {name} = req.body
+    const result = await user.insertOne({name: "John"});
+    return res.json(result);
+})
+
+app.get("/greet",(req,res)=>{res.send("Greetings")})
+
+app.get("/name",(req,res)=>{res.send("varshitha")})
+
+app.get("/weather",(req,res)=>{res.send("31degree")})
+
+app.get("/product",(req,res)=>{
+  const products =[
+    { name: "Product 1", price: 34},
+    { name: "Product 2", price: 64},
+    { name: "Product 3", price: 44},
+  ];
+  res.json(products);
+  
+})
+
+
+// import express from "express";
+// import cors from "cors";
+// const app = express();
+// app.listen(8080, () => {
+//   console.log("Server Started on port 8080");
+// });
+
+// app.use(express.json());
+// app.use(cors());
+// const users = [];
+
+// app.get("/", (req, res) => {
+//   return res.json(users);
+// });
+
+// app.post("/register", (req, res) => {
+//   const { name, email, pass } = req.body;
+//   users.push({ name, email, pass });
+//   return res.json(users);
+// });
+
+// app.post("/login", (req, res) => {
+//   const { email, pass } = req.body;
+//   const found = users.find(
+//     (value) => value.email === email && value.pass === pass
+//   );
+//   if (!found) {
+//     res.json({message:"User not found"})
+//   }
+//   res.json({...found,token:"123"});
+// });
