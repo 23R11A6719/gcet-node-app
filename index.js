e40ba52d35bf7cc5652229bc65f45375743a9c1b
23R11A6719 <23r11a6719@gcet.edu.in>
2025-06-04 14:13:16 +0530

    update

--- index.js
+++ index.js
@@ -2,76 +2,62 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import userRouter from "./routes/userRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.listen(8080, () => {
-    mongoose.connect("mongodb://localhost:27017/gcet");
-  console.log("Server Started on port 8080");
+const MONGO_URI = process.env.MONGO_URI
+
+app.use("/users", userRouter);
+app.use("/products", productRouter);
+app.use("/orders",orderRouter)
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server Started on port 8080");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+//app.listen(8080, () => {
+//   mongoose.connect(`${MONGODB_URI}`);
+//   console.log("Server Started");
+// });
+
+
+/**app.get("/", (req, res)=>{
+  return res.send(`<h1>Welcome to the API Index</h1>
+    <ol>
+      <li><a href="/greet">/greet</a></li>
+      <li><a href="/name">/name</a></li>
+      <li><a href="/weather">/weather</a></li>
+      <li><a href="/products">/products</a></li>
+      <li><a href="/register">/register</a></li>
+      <li><a href="/login">/login</a></li>
+    </ol>
+  `);
+   res.send("Good Morning!!");
 });
 
-const userSchema = mongoose.Schema({
-    name: {type: String},
+app.get("/greet", (req, res)=>{
+  res.send("Greetings!!");
+} );
+
+app.get("/name", (req, res)=>{
+  res.send("varshitha");
+} );
+
+app.get("/weather", (req, res)=>{
+  res.send("29 degrees");
 });
 
-const user = mongoose.model("User", userSchema);
-
-app.use(cors())
-app.use(express.json())
-app.get("/", async (req, res) => {
-  return res.send("Good Morning");
-});
-
-app.post("/register", async(req, res)=>{
-    const {name} = req.body
-    const result = await user.insertOne({name: "John"});
-    return res.json(result);
-})
-
-app.get("/greet",(req,res)=>{res.send("Greetings")})
-
-app.get("/name",(req,res)=>{res.send("varshitha")})
-
-app.get("/weather",(req,res)=>{res.send("31degree")})
-
-app.get("/product",(req,res)=>{
-  const products =[
-    { name: "Product 1", price: 34},
-    { name: "Product 2", price: 64},
-    { name: "Product 3", price: 44},
-  ];
-  res.json(products);
-  
-})
-
-
-// import express from "express";
-// import cors from "cors";
-// const app = express();
-// app.listen(8080, () => {
-//   console.log("Server Started on port 8080");
-// });
-
-// app.use(express.json());
-// app.use(cors());
-// const users = [];
-
-// app.get("/", (req, res) => {
-//   return res.json(users);
-// });
-
-// app.post("/register", (req, res) => {
-//   const { name, email, pass } = req.body;
-//   users.push({ name, email, pass });
-//   return res.json(users);
-// });
-
-// app.post("/login", (req, res) => {
-//   const { email, pass } = req.body;
-//   const found = users.find(
-//     (value) => value.email === email && value.pass === pass
-//   );
-//   if (!found) {
-//     res.json({message:"User not found"})
-//   }
-//   res.json({...found,token:"123"});
-// });
+**/
